cff4f7fcb4dc9bb306c6c3ee79dee3b8a6547787
Oleg Ionov <ionov.dpmn@gmail.com>
2024-03-12 12:51:59 +0300

    Добавлены схемы объектов API

--- vk_api_ads/__init__.py
+++ vk_api_ads/__init__.py
@@ -1,11 +1,21 @@
 from vk_api_ads.client import VKAds
 from vk_api_ads.app import VKApp
 from vk_api_ads.constants import IDType, StatisticsPeriod
-
+from vk_api_ads.schemas import (CampaignSchema,
+                                AdSchema,
+                                AdLayoutSchema,
+                                StatisticSchema)
 
 __all__ = [
+    # Клиенты
     'VKApp',
     'VKAds',
+    # Константы
     'IDType',
     'StatisticsPeriod',
+    # Схемы
+    'CampaignSchema',
+    'AdSchema',
+    'AdLayoutSchema',
+    'StatisticSchema',
 ]
